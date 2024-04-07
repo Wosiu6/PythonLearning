a171df6aed2852b214faed10a4fab0a720519bce
wosiu6 <wos.patryk3@gmail.com>
2024-04-07 09:30:15 +0100

    softmax activation func

--- NeuralNetwork/dense_layer_example.py
+++ NeuralNetwork/dense_layer_example.py
@@ -4,8 +4,6 @@ from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-X,y = spiral_data(100, 3)
-
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons) # inverse sizes to negate the need of Transposing #
@@ -16,12 +14,25 @@ class Layer_Dense:
 class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
+   
+class Actiovation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
 
-layer1 = Layer_Dense(2, 5)
+X,y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
+dense2 = Layer_Dense(3, 3)
+activation2 = Actiovation_Softmax()
 
-print(layer1.output)
-print(activation1.output)
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output)
